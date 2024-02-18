6ea311ad5f9a709e6b5601a1ddbc0ae268a43e4b
Mohini <upretimohini@gmail.com>
2024-02-18 22:38:23 +0530

    Update the styling

--- main.js
+++ main.js
@@ -2,6 +2,23 @@ import './style.css'
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
+const textToType = `Hi! I am Mohini ,\n a front-end developer`;    
+const typingSpeed = 100 ;
+const typingTextElement = document.getElementById('home');
+
+function typeText(){
+  let index = 0 ;
+  const typingInterval = setInterval( ()=>{
+    typingTextElement.textContent = textToType.slice(0,index);
+    index++;
+
+    if(index >textToType.length) {
+      clearInterval(typingInterval);
+    }
+
+  } , typingSpeed);
+}
+window.onload = typeText;
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight ,0.1,1000);
@@ -13,7 +30,7 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);  // to have it render on the full screen
-camera.position.setZ(30);  
+camera.position.setZ(40);  
 camera.position.setX(-3);
 
 renderer.render(scene, camera);
@@ -23,29 +40,32 @@ renderer.render(scene, camera);
 
 //now create the object , three js has many geometry objects in their documentation , use them , they have dimaension specified as coordinates
 
-const geometry = new THREE.TorusGeometry(20, 3, 16, 100 );  //constructor with arguments
-//we need to provide it material now , its like wrapping paper for an object 
-// const material = new THREE.MeshStandardMaterial(
-//   {color : '#966fd6' } );  // We can use WebGL to make our own custom shaders , no light source in this basic one is MeshBasicMaterial , light required for MeshStandardMaterial.
 
-  // now create mesh with geometry and the material 
 
-const torusTexture  = new THREE.TextureLoader().load('silver_sparkle.jpg');
-new THREE.MeshBasicMaterial({
-  map: torusTexture
-})
-const torus = new THREE.Mesh(
- geometry,
-  new THREE.MeshBasicMaterial({
-    map: torusTexture
-  })
-  );
- 
-scene.add(torus);   // to see it we are adding the mesh on the scene .
-const pointLight = new THREE.PointLight(0xffffff);  // point light is like light in all directions ,like a ligt bulb , give positions to the light , its in the center right now when just instantiated .
+
+  //navbar 
+
+  document.addEventListener('DOMContentLoaded' , ()=>{
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll' , ()=> {
+      const scrollPosition = window.scrollY;
+      const navbarHeight = navbar.offsetHeight ;
+
+      if(scrollPosition > navbarHeight){
+        document.body.classList.add('navbar-scrolled');
+      }else{
+        document.body.classList.remove('navbar-scrolled');
+      }
+    })
+  } )
+
+
+
+const pointLight = new THREE.PointLight(0xff00ff);  // point light is like light in all directions ,like a ligt bulb , give positions to the light , its in the center right now when just instantiated .
 pointLight.position.set(5, 5, 5 );
 
-const ambientLight = new THREE.AmbientLight(0xffffff);
+const ambientLight = new THREE.AmbientLight(0xff00ff);
 scene.add(pointLight, ambientLight);
 
 const lightHelper = new THREE.PointLightHelper(pointLight);
@@ -57,10 +77,15 @@ const controls = new OrbitControls(camera , renderer.domElement);
 
 // instead of calling render method this many times, we can use a recursive function , which will use animate function that tells the browser that there is an animation and this will call the render method automatically , its like a game loop(game developmentreference.)
 
+//STARS
+
+const starsGroup = new THREE.Group();
+scene.add(starsGroup);
+
 function addStar(){
   const geometry = new THREE.SphereGeometry(0.25 , 24 , 24);
   const material = new THREE.MeshStandardMaterial( {
-    color : 0xffffff
+    color : '#FFFFFF'
   });
   const star = new THREE.Mesh(geometry, material);
 
@@ -68,55 +93,44 @@ function addStar(){
   const[x,y,z] =Array(3).fill().map(()=> THREE.MathUtils.randFloatSpread(100));
 
   star.position.set(x,y,z);
-  scene.add(star);
+  starsGroup.add(star);
 
 }
-Array(200).fill().forEach(addStar);    //to specify the amount of stars needed 
+Array(400).fill().forEach(addStar);    //to specify the amount of stars needed 
 
-const spaceTexture = new THREE.TextureLoader().load('space2.jpg');
+const spaceTexture = new THREE.TextureLoader().load('black.png');
 scene.background = spaceTexture;
 
+//Avatar
 const MohiniTexture  = new THREE.TextureLoader().load('Background.png');
-
 const Mohini = new THREE.Mesh(
-new THREE.BoxGeometry(3, 3, 3),
+new THREE.BoxGeometry(20, 20, 20),
 new THREE.MeshBasicMaterial({
-  map: MohiniTexture
+  map: MohiniTexture ,
 })
 );
 scene.add(Mohini);
-
-
-//moon
-const moonTexture = new THREE.TextureLoader().load('moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('normal.jpg');
-const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(3,32,32),
-  new THREE.MeshStandardMaterial({
-    map : moonTexture,
-    normalMap : normalTexture
-  })
-);
-scene.add(moon);
-moon.position.z =30 ;
-moon.position.setX(-10);
-
-Mohini.position.z = -5;
-Mohini.position.x = 2;
+Mohini.position.z = -2;
+Mohini.position.y = 3;
+Mohini.position.x = 20;
 
 function moveCamera(){
 const t = document.body.getBoundingClientRect().top;
-moon.rotation.x += 0.05 ; 
-moon.rotation.y += 0.75 ; 
-moon.rotation.z += 0.05 ; 
 
-Mohini.rotation.y += 0.01 ;
-Mohini.rotation.z += 0.01 ;
 
-camera.position.z = t * -0.01;
-camera.position.x = t * -0.0002;
-camera.position.y = t * -0.0002;
+// Mohini.rotation.y += 0.01 ;
+if(t<0){
+  // Rotate the avatar around the z-axis
+  Mohini.rotation.z = t * 0.001;
 
+  // Move the avatar inwards along the z-axis
+  Mohini.position.z = t * 0.1;
+// Mohini.rotation.z += 0.01 ;
+
+// camera.position.z = t * -0.01;
+// camera.position.x = t * -0.0002;
+// camera.position.y = t * -0.0002;
+}
 
 }
 
@@ -137,14 +151,16 @@ window.addEventListener('resize', handleResize);
 
 function animate(){
   requestAnimationFrame(animate);
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
-  
-  // Mohini.rotation.y += 0.005;
-  moon.rotation.x += 0.005;
-controls.update();
 
+  
+// Mohini.rotation.y += 0.005;
+ 
+ controls.update();
+ starsGroup.children.forEach(star => {
+  star.position.x += 0.1 * Math.sin(Date.now() * 0.001);
+  star.position.y += 0.1 * Math.sin(Date.now() * 0.001);
+ });
   renderer.render(scene, camera);
+ 
 }
 animate();
